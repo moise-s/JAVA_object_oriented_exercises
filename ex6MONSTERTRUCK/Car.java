1a18e1bb3cd2aaa341188eadd2065dc4b6168bc8
Moisés do Nascimento <moises.d.nasci@gmail.com>
2022-05-24 17:33:53 -0300

    added ex6

---
+++ ex6MONSTERTRUCK/Car.java
@@ -0,0 +1,16 @@
+package ex6MONSTERTRUCK;
+
+public class Car {
+  public void m1() {
+    System.out.println("car 1");
+  }
+
+  public void m2() {
+    System.out.println("car 2");
+  }
+
+  @Override
+  public String toString() {
+    return "vruum";
+  }
+}
---
+++ ex6MONSTERTRUCK/Main.java
@@ -0,0 +1,16 @@
+package ex6MONSTERTRUCK;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Car car = new Car();
+    Truck truck = new Truck();
+    MonsterTruck monsterTruck = new MonsterTruck();
+
+    monsterTruck.m1();
+    monsterTruck.m2();
+    System.out.println(monsterTruck);
+
+  }
+}
